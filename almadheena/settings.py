943a8a5337846894c3155b0f5041a9a493e5208e
Rashidkv7 <muhammedrashid8448@gmail.com>
2022-02-17 14:09:18 +0530

    added allowed host

--- almadheena/settings.py
+++ almadheena/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#nvlvpq4*n4ayshzqs)_+j260a+)!c1ji71!bs-()s$ii6b%!q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
